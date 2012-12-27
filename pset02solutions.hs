0e4f592b83af31dfca9a3bd9c160b30bdd93bd10
Lennart Jansson <lennartj@stanford.edu>
2012-12-27 10:41:56 -0800

    New recursion problem, update solutions, remove Vigenere from 3

--- pset02solutions.hs
+++ pset02solutions.hs
@@ -38,6 +38,29 @@ init' (x:xs) = x:(init' xs)
 cycle' :: [a] -> [a]
 cycle' xs = xs ++ cycle' xs
 
+mergesort :: Ord a => [a] -> [a]
+mergesort [] = []
+mergesort [x] = [x]
+mergesort xs =
+  let mid = (length xs) `div` 2
+      (xs1, xs2) = splitAt mid xs
+  in merge (mergesort xs1) (mergesort xs2)
+  where merge [] bs = bs
+        merge as [] = as
+        merge allas@(a:as) allbs@(b:bs)
+          | a < b = a : (merge as allbs)
+          | otherwise = b : (merge allas bs)
+
+sum2 :: Num a => [a] -> a
+sum2 ns = sum2' ns 0
+  where sum2' [] s = s
+        sum2' (n:ns) s = sum2' ns (s + n)
+
+reverse2 :: [a] -> [a]
+reverse2 xs = reverse2' xs []
+  where reverse2' [] bs = bs
+        reverse2' (f:fs) bs = reverse2' fs (f:bs)
+
 ppParens :: String -> String
 ppParens p = reverse (tail (ppParens' p 0 ""))
   where ppParens' "" _ s = s
@@ -45,3 +68,27 @@ ppParens p = reverse (tail (ppParens' p 0 ""))
           ppParens' ps (n+1) ("\n(" ++ (replicate n '\t') ++ s)
         ppParens' (')':ps) n s =
           ppParens' ps (n-1) ("\n)" ++ (replicate (n-1) '\t') ++ s)
+
+-- | CHALLENGE PROBLEM
+-- Several bits could be a little bit nicer with higher-order functions, but
+-- this is all doable with what we've got. Lazy evaluation!
+
+coprimePerm :: [Int] -> [Int]
+coprimePerm ns = 
+  case (coprimePerm' [] ns []) of
+    [] -> [-1]
+    rs -> reverse (head rs)
+
+coprimePerm' :: [Int] -> [Int] -> [[Int]] -> [[Int]]
+coprimePerm' built [] results = built : results
+coprimePerm' built remain results =
+  let selects = [ns | ns@(n, _) <- selectOff remain,
+                 null built || coprime n (head built)]
+  in concat [coprimePerm' (n:built) l results | (n, l) <- selects]
+
+selectOff :: [Int] -> [(Int, [Int])]
+selectOff xs = [(b, fs ++ bs) | n <- [0..(length xs - 1)],
+                let (fs, (b:bs)) = splitAt n xs]
+
+coprime :: Int -> Int -> Bool
+coprime a b = (gcd a b) == 1
